bbe720f2a104761e40fefe194a072c35d11082ea
RobertEli <57449733+RobertElikplim@users.noreply.github.com>
2022-04-01 21:47:47 -0500

    update

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,7 @@ import Router from "./routes/routes.js";
 
 // init express
 const app = express();
+const port = process.env.PORT || 5000;
 
 // use express json
 app.use(express.json());
@@ -18,9 +19,17 @@ app.use(cors());
 // use router
 app.use(Router);
 
+// To serve our frontend
+app.get(/.*/, (_req, res) => {
+    const p = path.resolve(__dirname, '../frontend/dist/index.html');
+    res.sendFile(p);
+});
+
 app.use((_req, res) => {
     res.status(404).send("<h1>unable to find that</h1>");
 });
 
 
-app.listen(process.env.PORT, '0.0.0.0', () => console.log("Server running at " + process.env.PORT)); 
+app.listen(port, function () {
+    console.log("Ready to Go! Backend listening on port : " + port);
+  });
